2a19a8940899c4d0d003f799669f04aa3e4a75f2
Scott Gigante <gigante.s@wehi.edu.au>
2017-06-23 15:34:59 +1000

    add gmap scripts

---
+++ gmap_run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS -q small
+
+# Usage: qsub -F "wehi_home/grpu_mritchie_0/AGRF_Data/2017_05_09/reads/fasta/albacore_no_phage.fasta" gmap_run.sh
+
+set -x
+FASTA=$1
+FMT=$(echo $FASTA | sed 's/.*\.//')
+N=2000
+FASTA_DIR=$(dirname $FASTA)
+TMP_DIR=tmp/gmap/$FASTA_DIR
+SCRIPTS_DIR=$HOME/nanopore-scripts
+mkdir -p $TMP_DIR
+cp $FASTA $TMP_DIR/gmap.$FMT
+(cmp --silent $FASTA $TMP_DIR/gmap.$FMT && echo "Copied successfully to HPC") || (echo "ERROR: Copying fasta to HPC failed" && exit 1)
+
+cd $TMP_DIR
+python $SCRIPTS_DIR/split_fasta.py gmap.$FMT $N $FMT
+NUM_JOBS=$(ls -1 gmap.${FMT}.*.$FMT | wc -l)
+qsub -t 1-$NUM_JOBS -F "$TMP_DIR $FMT" $SCRIPTS_DIR/gmap.sh
+
