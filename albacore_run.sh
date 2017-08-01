37f89d790939da2a62885e685e4357bb47c3d351
Scott Gigante <scottgigante@gmail.com>
2017-08-01 14:28:10 +1000

    improve fastq handling

--- albacore_run.sh
+++ albacore_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Usage: ./albacore_run.sh [-h] [-i INPUT_DIR] [-o OUTPUT_DIR] [-c CONFIG] /path/to/fast5
-# Defaults: -i raw -o albacore -c r94_450bps_linear.cfg
+# Usage: ./albacore_run.sh [-h] [-i INPUT_DIR] [-o OUTPUT_DIR] [-c CONFIG] [-5] [-q] /path/to/fast5
+# Defaults: -i raw -o albacore -c r95_450bps_linear.cfg
 # Directory structure: 
 # Raw reads:  /path/to/fast5/raw
 # Basecalled: /path/to/fast5/albacore
@@ -18,14 +18,14 @@ if [ $(pip freeze | grep -c "ont-albacore") -eq 0 ]; then
 fi
 
 # Set default arguments
-RAW="raw"
-READS="albacore"
+INPUT="raw"
+OUTPUT="albacore"
 CONFIG="r95_450bps_linear.cfg"
 MODE="fast5"
 FAST5=false
 FASTQ=false
 HELP="albacore_run.sh: Albacore wrapper for Milton
-Scott Gigante, 2017-06-23
+Scott Gigante, 2017-08-01
 Usage: ./albacore_run.sh [-h] [-i INPUT_DIR] [-o OUTPUT_DIR] [-c CONFIG] [-5] [-q] /path/to/fast5"
 
 # Parse command line arguments. If an argument is missing, it will be left as default
@@ -33,16 +33,14 @@ while getopts 'hi:o:c:5q' arg
 do
   case ${arg} in
     h) echo "$HELP"; exit 0;;
-    i) RAW=${OPTARG};;
-    o) READS=${OPTARG};;
+    i) INPUT=${OPTARG};;
+    o) OUTPUT=${OPTARG};;
     c) CONFIG=${OPTARG};;
     5) FAST5=true;;
     q) FASTQ=true;;
     *) echo "$HELP" >&2; exit 1 # illegal option
     esac
 done
-# Shift the beginning argument index so we only have what hasn't been parsed by getopt (eg, /path/to/fast5)
-shift $(($OPTIND - 1))
 # Set the output mode
 if $FAST5 && $FASTQ; then
   MODE="fast5,fastq"
@@ -52,6 +50,8 @@ elif $FAST5; then
   MODE="fast5"
 fi
 
+# Shift the beginning argument index so we only have what hasn't been parsed by getopt (eg, /path/to/fast5)
+shift $(($OPTIND - 1))
 # Check there is exactly one argument left
 if [ "$#" -eq 0 ]; then
   echo "ERROR:Directory argument is mandatory."
@@ -62,22 +62,22 @@ elif [ "$#" -gt 1 ]; then
   echo "$HELP" >&2
   exit 1
 fi
-# Set the fast5 parent directory to the absolute path of /path/to/reads
+# Set the fast5 parent directory to the absolute path of /path/to/fast5
 PARENT=$(realpath $1)
 
 # Check how many subfolders are in /path/to/fast5/raw
-NUM_RUNS=$(ls -1 $PARENT/$RAW | wc -l)
+NUM_RUNS=$(ls -1 $PARENT/$INPUT | wc -l)
 # Make the output directory, if it doesn't already exist
-mkdir -p $PARENT/$READS
+mkdir -p $PARENT/$OUTPUT
 # Print some run information
 echo "albacore_run.sh: Albacore wrapper for Milton
 Scott Gigante, 2017-06-23
 
-Input: $PARENT/$RAW
-Output: $PARENT/$READS
+Input: $PARENT/$INPUT
+Output: $PARENT/$OUTPUT
 Config: $CONFIG
 Output: $MODE
 Jobs: $NUM_RUNS"
 # Run the array job
-qsub -t 1-$NUM_RUNS -F "$PARENT $RAW $READS $CONFIG $MODE" $SCRIPTS_DIR/albacore.sh
+qsub -t 1-$NUM_RUNS -F "$PARENT $INPUT $OUTPUT $CONFIG $MODE" $SCRIPTS_DIR/albacore.sh
 
