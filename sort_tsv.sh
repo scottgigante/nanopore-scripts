65d4df56902171d344f4c34c62187dfc178346ab
Scott Gigante <scottgigante@gmail.com>
2017-08-08 14:54:22 +1000

    add sort_tsv.sh

---
+++ sort_tsv.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Sort a tsv file without destroying headers
+# Assumes headers start with a hash and nothing else does
+# Usage: ./sort_tsv.sh infile.tsv <args for sort>
+# Help: `man sort`
+INFILE=$1
+shift 1
+ARGS=$@
+
+# check header length
+i=0
+while [ true ]; do
+  if [ $(head -n $i $INFILE | grep -c -e "^#") -lt $i ]; then
+    i=$(($i-1))
+    break
+  fi
+  i=$(($i+1))
+done
+
+set -x
+tail -n +$(($i+1)) $INFILE | sort $ARGS | cat <(head -n $i $INFILE) -
