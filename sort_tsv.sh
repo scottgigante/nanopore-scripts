fe22fe8efeb0d2cb81c4fa06ff7b26367883c9d5
Scott Gigante <scottgigante@gmail.com>
2017-08-09 14:48:29 +1000

    allow manual specification of header length

--- sort_tsv.sh
+++ sort_tsv.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 # Sort a tsv file without destroying headers
-# Assumes headers start with a hash
-# Usage: ./sort_tsv.sh infile.tsv <args for sort>
+# Assumes headers start with a hash or length of header is provided by --skip
+# Usage: ./sort_tsv.sh infile.tsv [--skip n] <args for sort>
 # Help: `man sort`
 INFILE=$1
-shift 1
+if [ $2 == "--skip" ]; then
+  i=$3
+  shift 3
+else
+  # check header length
+  i=0
+  while [ true ]; do
+    if [ $(head -n $i $INFILE | grep -c -e "^#") -lt $i ]; then
+      i=$(($i-1))
+      break
+    fi
+    i=$(($i+1))
+  done
+  shift 1
+fi
 ARGS=$@
 
-# check header length
-i=0
-while [ true ]; do
-  if [ $(head -n $i $INFILE | grep -c -e "^#") -lt $i ]; then
-    i=$(($i-1))
-    break
-  fi
-  i=$(($i+1))
-done
-
-set -x
 tail -n +$(($i+1)) $INFILE | sort $ARGS | cat <(head -n $i $INFILE) -
